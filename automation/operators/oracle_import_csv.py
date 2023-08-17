72fb3e8cbd5228e105393ac654a299e82b989ecd
Mauricio de Moraes <71461873+moraesmm@users.noreply.github.com>
2023-08-16 21:39:21 -0300

    feat: Add CSV Import Functionality for Oracle Database

--- automation/operators/oracle_import_csv.py
+++ automation/operators/oracle_import_csv.py
@@ -1,5 +1,39 @@
-def execute(csv_folder,db_host,db_port,db_name,db_usr,db_pwd,db_schema,db_table,tb_columns):
+def execute(csv_folder, db_host, db_port, db_name, db_usr, db_pwd, db_schema, db_table, tb_columns):
+    """
+    Import CSV files into an Oracle database.
 
+    This function reads CSV files from a specified folder and inserts their data into an Oracle database table.
+    
+    Args:
+        csv_folder (str): The folder path containing the CSV files to be imported.
+        db_host (str): The hostname or IP address of the Oracle database.
+        db_port (int): The port number of the Oracle database.
+        db_name (str): The SID or Service Name of the Oracle database.
+        db_usr (str): The username for connecting to the Oracle database.
+        db_pwd (str): The password for the Oracle user.
+        db_schema (str): The schema of the Oracle database where the table resides.
+        db_table (str): The name of the Oracle table where data will be inserted.
+        tb_columns (str): A comma-separated list of columns in the Oracle table.
+        
+    Notes:
+        - This function assumes that the first row of each CSV file is a header and will be skipped during import.
+        - The Oracle connection is managed using SQLAlchemy and the cx_Oracle driver.
+
+    Example:
+        execute(
+            csv_folder='/path/to/csv_files',
+            db_host='localhost',
+            db_port=1521,
+            db_name='ORCL',
+            db_usr='myuser',
+            db_pwd='mypassword',
+            db_schema='myschema',
+            db_table='mytable',
+            tb_columns='col1, col2, col3'
+        )
+    """
+    
+    # Import necessary libraries
     from sqlalchemy import create_engine, text
     from sqlalchemy.exc import SQLAlchemyError
     import os
@@ -7,56 +41,48 @@ def execute(csv_folder,db_host,db_port,db_name,db_usr,db_pwd,db_schema,db_table,
     import pandas as pd
     import csv
     from datetime import datetime
-
-
-    """
-        Use this function to import a csv file into oracle db.
-
-        Args:
-            csv_folder: Define folder to read csv files
-            db_host: Oracle host
-            db_port: Oracle port
-            db_name: Oracle SID or Service Name
-            db_usr: Oracle user
-            db_pwd: Oracle password
-            db_schema: Oracle schema
-            db_table: Oracle table (destination)
-            tb_columns: Define columns to insert data
-    """
-    tb_columns = str(tb_columns).replace("'","")
-
-    # get a list of csv files
+    
+    # Prepare table columns for SQL insertion
+    tb_columns = str(tb_columns).replace("'", "")
+    
+    # Get a list of CSV files in the specified folder
     csv_files = [file for file in os.listdir(csv_folder) if file.endswith(".csv")]
-
-    # create oracle conn 
-    # study the best way to handle with oracle conn and implement it
+    
     try:
+        # Connect to Oracle using SID (if possible)
         conn = create_engine(f'oracle+cx_oracle://{db_usr}:{db_pwd}@{db_host}:{db_port}/{db_name}')
         conn = conn.connect()
     except SQLAlchemyError as e:
+        # If SID connection fails, use Service Name
         error = str(e.__dict__['orig'])
         conn = create_engine(f'oracle+cx_oracle://{db_usr}:{db_pwd}@{db_host}:{db_port}/?service_name={db_name}')
         conn = conn.connect()
 
-    # list csv files in csv folder and insert them into oracle
+    # Loop through each CSV file and import its data
     for csv_file_name in csv_files:
-        csv_path = os.path.join(csv_folder,csv_file_name)
+        csv_path = os.path.join(csv_folder, csv_file_name)
 
         with open(csv_path, "r") as csv_file:
-            csv_reader=csv.reader(csv_file)
-            next(csv_reader) # skip header row
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)  # Skip header row
 
             for row in csv_reader:
-                now=datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-                now=f'to_timestamp(\'{str(now)}\', \'YYYY-MM-DD HH24:MI:SS.XFF\')'
+                # Generate current timestamp for insertion
+                now = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                now = f'to_timestamp(\'{str(now)}\', \'YYYY-MM-DD HH24:MI:SS.XFF\')'
+                
+                # Prepare SQL insert statement
                 sql_insert = f'INSERT INTO {db_schema}.{db_table} ({tb_columns}) VALUES ({str(row[1:]).replace("  ","")},{now})'
-                sql_insert = str(sql_insert).replace("[","").replace("]","")
+                sql_insert = str(sql_insert).replace("[", "").replace("]", "")
+                
                 try:
+                    # Execute SQL insert statement
                     conn.execute(text(sql_insert))
-                    # create and implement function to delete csv file
+                    # Additional logic to delete the CSV file can be added here
                 except SQLAlchemyError as e:
                     error = str(e.__dict__['orig'])
-                    # create function to insert the log msg into oracle db
+                    # Additional logic to insert the error log into Oracle database can be added here
                     print(f'######## ERROR!!! ########\n{error}\nConnecton-> {conn}') 
 
+    # Close the Oracle connection
     conn.close()
